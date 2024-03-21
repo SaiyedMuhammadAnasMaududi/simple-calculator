04c0bbb71a942482bd466e2e1eac943d9e2f248e
SaiyedMuhammadAnasMaududi <161339637+SaiyedMuhammadAnasMaududi@users.noreply.github.com>
2024-03-21 13:27:38 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-#!/usr/bin/enr node
+#!/usr/bin/env node
 import inquirer from 'inquirer';
 const answer = await inquirer.prompt([{ message: "ENTER FIRST NUMBER", type: "number", name: "firstnumber" }, { message: "Enter  second number", type: "number", name: "secondnumber" }, { message: "select the operator to perform action", type: "list", name: "operator", choices: ["Addition", "Subtraction", "Division", "Multiplication",] }]);
 if (answer.operator === "Addition") {
