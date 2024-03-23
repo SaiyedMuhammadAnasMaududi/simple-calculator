ee28a622533761dbda57535eb94ada154bb9b598
SaiyedMuhammadAnasMaududi <tafs4920@outlook.com>
2024-03-23 17:11:17 +0500

    alright now

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-#!/usr/bin/env node
+#! /usr/bin/env node
 import inquirer from 'inquirer';
-const answer = await inquirer.prompt([{ message: "ENTER FIRST NUMBER", type: "number", name: "firstnumber" }, { message: "Enter  second number", type: "number", name: "secondnumber" }, { message: "select the operator to perform action", type: "list", name: "operator", choices: ["Addition", "Subtraction", "Division", "Multiplication",] }]);
+const answer = await inquirer.prompt([
+    { message: "ENTER FIRST NUMBER",
+        type: "number",
+        name: "firstnumber" },
+    { message: "Enter  second number",
+        type: "number",
+        name: "secondnumber" },
+    { message: "select the operator to perform action",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Division", "Multiplication",] }
+]);
 if (answer.operator === "Addition") {
     console.log(answer.firstnumber + answer.secondnumber);
 }
--- index.ts
+++ index.ts
@@ -1,7 +1,26 @@
-#!/usr/bin/env node
+#! /usr/bin/env node
+
 import inquirer from 'inquirer'
-const answer = await inquirer.prompt([{message:"ENTER FIRST NUMBER",type:"number",name:"firstnumber"},{message:"Enter  second number", type:"number",name:"secondnumber"},{message :"select the operator to perform action",type:"list",name:"operator",choices:["Addition","Subtraction","Division", "Multiplication", ]}]);
-if (answer.operator==="Addition"){console.log(answer.firstnumber + answer.secondnumber)}
- else if(answer.operator==="Subtraction"){console.log(answer.firstnumber - answer.secondnumber)}
- else if(answer.operator==="Multiplication"){console.log(answer.firstnumber * answer.secondnumber)}
- else{console.log(answer.firstnumber/answer.secondnuber)};
+
+
+const answer = await inquirer.prompt([
+    {message:"ENTER FIRST NUMBER",
+    type:"number",
+    name:"firstnumber"},
+    {message:"Enter  second number", 
+    type:"number",
+    name:"secondnumber"},
+    {message :"select the operator to perform action",
+    type:"list",
+    name:"operator",
+    choices:["Addition","Subtraction","Division", "Multiplication", ]}]);
+
+            if (answer.operator==="Addition"){
+                console.log(answer.firstnumber + answer.secondnumber)
+            } else if(answer.operator==="Subtraction"){
+                console.log(answer.firstnumber - answer.secondnumber)
+            } else if(answer.operator==="Multiplication"){
+                console.log(answer.firstnumber * answer.secondnumber)
+            } else{
+                console.log(answer.firstnumber/answer.secondnuber)
+            };
